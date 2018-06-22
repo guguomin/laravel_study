f0e4946b0576aa27471605462bb1cfd01af73f6e
guguomin <407006576@qq.com>
2018-06-22 16:55:32 +0800

    静态页面构建

---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\request;
+
+class StaticPagesController extends Controller
+{
+    public function home()
+    {
+        return view('static_pages/home');
+    }
+
+    public function help()
+    {
+        return view('static_pages/help');
+    }
+
+    public function about()
+    {
+        return view('static_pages/about');
+    }
+}
